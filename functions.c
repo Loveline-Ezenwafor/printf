8ccdcc460c8104f6cc6ba1eec2d2fb5991ef6c14
Iwuchukwu Alexander <iwuchukwualexander@gmail.com>
2023-04-24 03:26:37 -0700

    functions.c

--- functions.c
+++ functions.c
@@ -4,15 +4,12 @@
 
 /**
  * print_char - Prints a char
- *
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: Width
- *
  * @precision: Precision specification
  * @size: Size specifier
- *
  * Return: Number of chars printed
  */
 int print_char(va_list types, char buffer[],
@@ -27,12 +24,10 @@ int print_char(va_list types, char buffer[],
  * print_string - Prints a string
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
- *
  * @flags:  Calculates active flags
  * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
- *
  * Return: Number of chars printed
  */
 int print_string(va_list types, char buffer[],
@@ -83,14 +78,11 @@ int print_string(va_list types, char buffer[],
 /**
  * print_percent - Prints a percent sign
  * @types: Lista of arguments
- *
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width.
- *
  * @precision: Precision specification
  * @size: Size specifier
- *
  * Return: Number of chars printed
  */
 int print_percent(va_list types, char buffer[],
